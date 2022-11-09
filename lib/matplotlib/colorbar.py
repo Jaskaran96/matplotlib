86121aee1ec65a8213b4883150ac6b726759c460
Jaskaran Singh <bartychuck@gmail.com>
2022-11-09 12:15:51 +0530

    Changed colorbar.py

--- lib/matplotlib/colorbar.py
+++ lib/matplotlib/colorbar.py
@@ -773,7 +773,7 @@ class Colorbar:
         self.lines.append(col)
 
         # make a clip path that is just a linewidth bigger than the axes...
-        if(len(linewidths)>0):
+        if(len(linewidths)>=1):
             fac = np.max(linewidths) / 72
             xy = np.array([[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]])
             inches = self.ax.get_figure().dpi_scale_trans
