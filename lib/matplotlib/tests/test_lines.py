7272554466a6aae5f9f2081026715b361500109c
harshschauhan <harshschauhan07102000@gmail.com>
2022-11-10 19:59:44 +0530

    Removed unnecessary tests

--- lib/matplotlib/tests/test_lines.py
+++ lib/matplotlib/tests/test_lines.py
@@ -21,6 +21,12 @@ import matplotlib.transforms as mtransforms
 from matplotlib.testing.decorators import image_comparison, check_figures_equal
 
 
+@image_comparison(baseline_images=['line_dashes'], remove_text=True,
+                  extensions=['png'])
+def test_line_dashes():
+    fig, ax = plt.subplots()
+    ax.plot(range(10), linestyle=(0, (3, 3)), lw=5)
+
 def test_segment_hits():
     """Test a problematic case."""
     cx, cy = 553, 902
