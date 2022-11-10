e1487b8b892e6f9ae4aac27a1afee69e8c8dc91c
harshschauhan <harshschauhan07102000@gmail.com>
2022-11-10 22:32:00 +0530

    Added another test for scatter plot

--- lib/matplotlib/tests/test_colors.py
+++ lib/matplotlib/tests/test_colors.py
@@ -38,6 +38,19 @@ def test_scatter_bug3():
     ax.scatter(np.random.rand(500)-0.35, np.random.rand(500), label="Second dataset")
     ax.legend(labelcolor="linecolor")
 
+
+@image_comparison(baseline_images=['scatter_bug3_2'], remove_text=False,
+                  extensions=['png'])
+def test_scatter_bug3_2():
+    x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+    y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+    plt.scatter(x, y, color = 'hotpink')
+
+    x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+    y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+    plt.scatter(x, y, color = '#88c999')
+
+
 def test_resampled():
     """
     GitHub issue #6025 pointed to incorrect ListedColormap.resampled;
